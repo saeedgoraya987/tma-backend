7224f97b7ea78d92f492990a0bc516e7507c8a24
kidkender <kidkender@gmail.com>
2024-07-20 20:34:59 +0700

    add rule skip browser warning

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,15 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors({
+    origin: '*',
+    methods: ['GET', 'POST', 'OPTIONS', 'PUT', 'PATCH', 'DELETE'],
+    allowedHeaders: [
+      'Content-Type',
+      'Authorization',
+      'ngrok-skip-browser-warning',
+    ],
+  });
   await app.listen(3000);
 }
 bootstrap();
